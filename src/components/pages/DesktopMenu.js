bf1775fe23c444b68a0770037bf63344defc5773
Shiho Nagano <shiho@nagano.com>
2021-12-31 11:04:46 +0900

    fixed menu

--- src/components/pages/DesktopMenu.js
+++ src/components/pages/DesktopMenu.js
@@ -11,7 +11,7 @@ export default function DesktopMenu({index, setIndex}) {
         <li className={index === 0 ? "nav-btn current" : "nav-btn"} onClick={() => {setIndex(0)}}>Home</li>
         <li className={(index * -1) / 100 === 1 ? "nav-btn current" : "nav-btn"} onClick={() => {setIndex(-100)}}>About</li>
         <li className={(index * -1) / 100 === 2 ? "nav-btn current" : "nav-btn"} onClick={() => {setIndex(-200)}}>Work</li>
-        <li className={(index * -1) / 100 === 3 ? "nav-btn current" : "nav-btn"} onClick={() => {setIndex(-300)}}>Resume</li>
+        <li className={(index * -1) / 100 === 3 ? "nav-btn current" : "nav-btn"} onClick={() => {setIndex(-300)}}>Résumé</li>
         <li className={(index * -1) / 100 === 4 ? "nav-btn current" : "nav-btn"} onClick={() => {setIndex(-400)}}>Contact</li>
       </ul>
     </nav>
